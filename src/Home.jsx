c9461f7c14caf481e358248d85bef42acb5bd255
ashrafomid2025 <omidmashraf@gmail.com>
2025-07-04 07:02:00 +0430

    error handler

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,5 +2,5 @@ import Fruits from "./components/Fruits.jsx";
 // import Animals from "./components/Animals.jsx";
 export default function Home() {
   const choice = "fruits";
-  return choice === "fruits" ? <Fruits /> : <h1>Nothing match</h1>;
+  return choice === "fruits" ? <Fruits /> : <h1>Something went wrong</h1>;
 }
